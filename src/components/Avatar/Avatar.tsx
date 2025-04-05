4831e291d63196a5841b48353cf7f9963338ed0e
mayonezny <borischubuhchiev@mail.ru>
2025-04-05 15:08:57 +0300

    added medals to runners

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,14 +1,20 @@
 import type React from 'react';
 import './avatar.scss';
+import { Place } from '../Place';
 
 interface AvatarPropsI {
   color: string;
   src: string;
+  place: number;
 }
 
-export const Avatar: React.FC<AvatarPropsI> = ({ color, src }) => {
+export const Avatar: React.FC<AvatarPropsI> = ({ color, src, place }) => {
   return (
     <div className="Avatar">
+      <Place
+        className="Avatar__Place"
+        place={place}
+      />
       <img
         className={`Avatar__img ${color}`}
         src={src}
--- src/widgets/RunnersList/RunnersList.tsx
+++ src/widgets/RunnersList/RunnersList.tsx
@@ -6,33 +6,37 @@ import './runners-list.scss';
 export const RunnersList: React.FC = () => {
   return (
     <div className="RunnersList">
-      <Paragraph level={1}>
-        <span>Участники</span>
-      </Paragraph>
+      <Paragraph level={1}>Участники</Paragraph>
       <div className="RunnersList__Avatars">
         <Avatar
           color="red"
           src="src/assets/images/person1.jpg"
+          place={1}
         />
         <Avatar
           color="brown"
           src="src/assets/images/person2.jpg"
+          place={2}
         />
         <Avatar
           color="violete"
           src="src/assets/images/person3.jpg"
+          place={3}
         />
         <Avatar
           color="blue"
           src="src/assets/images/person4.jpg"
+          place={4}
         />
         <Avatar
           color="green"
           src="src/assets/images/person5.jpg"
+          place={5}
         />
         <Avatar
           color="yellow"
           src="src/assets/images/person6.jpg"
+          place={6}
         />
       </div>
     </div>
